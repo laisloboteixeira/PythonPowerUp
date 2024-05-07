41952ed205c829db9e66170f9be064bcc4b76165
Laís Lobo Teixeira <kyliegomez120.n@outlook.com>
2024-05-06 21:54:32 -0300

    aula 01

---
+++ codigo.py
@@ -0,0 +1,56 @@
+# pip install pyautogui
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.3
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+
+pyautogui.click(x=674, y=405)
+pyautogui.write("lalaiteste@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("lalaiteste")
+
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+time.sleep(3)
+
+# pip install pandas numpy openpyxl
+import pandas as pd
+
+tabela = pd.read_csv("produtos.csv")
+
+print(tabela)
+
+print(tabela)
+
+for linha in tabela.index:
+
+    pyautogui.click(x=677, y=292)
+
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(str(codigo))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pd.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(5000)
